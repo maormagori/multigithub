95ffbaea169919fc33316daa04ded69c93983829
maormagori <maor@magori.online>
2023-07-01 19:26:46 +0300

    Added command to add new github user

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+import json
 import os
 import typer
+from typing_extensions import Annotated, Optional
+
 import repo_utils
 
+app = typer.Typer()
 
-def main(dir: str = os.getcwd()):
+
+@app.command("add-user")
+def add_user(
+        name: Annotated[str, typer.Option("--name", "-n")],
+        email: Annotated[str, typer.Option("--email", "-e")],
+        github_user: Annotated[str, typer.Option("--github-user", "-gh")]):
+    with open('users.json', 'r+') as f:
+        github_users = json.load(f)
+
+        if github_users.get(github_user) is not None:
+            print(f'User with the {github_user} github name already exists. overriding with new values...')
+        github_users[github_user] = {name: name, email: email}
+        f.seek(0)
+        json.dump(github_users, f, indent=4)
+
+
+@app.command()
+def use(ctx: typer.Context,
+        dir: Annotated[Optional[str], typer.Argument()] = os.getcwd(),
+        user: Annotated[Optional[str], typer.Option("--user", "-u")] = None):
     repo_utils.get_repo_owner(dir)
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
